3a0938b5efb70277ac81a2bcd825585fe9c80f9c
zxyao145 <zxyao145@gmail.com>
2023-08-15 00:04:21 +0800

    refactor: change rute info

--- src/BasicKube.Web/Common/Routes.cs
+++ src/BasicKube.Web/Common/Routes.cs
@@ -2,16 +2,19 @@
 
 public static class Routes
 {
-    public static string DeployGrpPage { get; } = "pages/deployAppGrpList";
-    public static string DaemonSetAppGrpPage { get; } = "pages/daemonSetAppGrpList";
-    public static string JobGrpPage { get; } = "pages/jobGrpList";
-    public static string CronJobGrpPage { get; } = "pages/cronJobGrpList";
+    #region kube
 
+    public static string DeployGrpPage { get; } = "pages/kube/deployAppGrpList";
+    public static string DaemonSetAppGrpPage { get; } = "pages/kube/daemonSetAppGrpList";
+    public static string JobGrpPage { get; } = "pages/kube/jobGrpList";
+    public static string CronJobGrpPage { get; } = "pages/kube/cronJobGrpList";
 
-    public static string SvcGrpPage { get; } = "pages/svcGrpList";
+    public static string SvcGrpPage { get; } = "pages/kube/svcGrpList";
 
-    public static string IngGrpPage { get; } = "pages/ingressGrpList";
+    public static string IngGrpPage { get; } = "pages/kube/ingressGrpList";
 
+    public static string TerminalPage { get; } = "pages/kube/terminal";
 
-    public static string TerminalPage { get; } = "pages/terminal";
+    #endregion
+
 }
